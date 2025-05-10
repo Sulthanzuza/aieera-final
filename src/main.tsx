808a0df51a4cb8e305886779daa7bd346fcdfe76
Sulthanzuza <sulthanpriv@gmail.com>
2025-05-10 13:06:33 +0530

    edited prerender

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,10 @@
-import { StrictMode } from 'react'; 
-import { createRoot, hydrateRoot } from 'react-dom/client'; 
-import App from './App.tsx'; 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 import './index.css';
 
-
-declare global {
-  interface Window {
-    snapSaveState?: () => string;
-  }
-}
-
-const rootElement = document.getElementById('root')!;
-
-if (rootElement.hasChildNodes()) {
-
-  hydrateRoot(
-    rootElement, 
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
-
-
-window.snapSaveState = () => {
-  return document.documentElement.outerHTML;
-};
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
