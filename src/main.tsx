9e0d53e4ee3fd51c844d0e91ec46cf2212e89bde
Sulthanzuza <sulthanpriv@gmail.com>
2025-05-10 12:16:44 +0530

    updated meta des

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react'; 
+import { createRoot, hydrateRoot } from 'react-dom/client'; 
+import App from './App.tsx'; 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+
+declare global {
+  interface Window {
+    snapSaveState?: () => string;
+  }
+}
+
+const rootElement = document.getElementById('root')!;
+
+if (rootElement.hasChildNodes()) {
+
+  hydrateRoot(
+    rootElement, 
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+
+window.snapSaveState = () => {
+  return document.documentElement.outerHTML;
+};
